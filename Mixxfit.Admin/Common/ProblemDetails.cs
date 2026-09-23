0693303389cd901572d5575333d9f4e3702a17f8
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-09-23 15:32:07 +0200

    feat(auth): Create globally accessible classes for api access and auth

---
+++ Mixxfit.Admin/Common/ProblemDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mixxfit.Admin.Common
+{
+    public record ProblemDetails
+    {
+        public int Status { get; init; }
+        public string Detail { get; init; } = string.Empty;
+        public string ErrorCode { get; init; } = string.Empty;
+        public string Title { get; init; } = string.Empty;
+        public string[] Errors { get; init; } = [];
+    }
+}
