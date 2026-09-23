bbabdb22720cd652cbe473d6cf0dd9ad070100f1
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-09-23 15:32:48 +0200

    feat(auth): Implement AuthService

---
+++ Mixxfit.Admin/Features/Auth/AuthResponse.cs
@@ -0,0 +1,13 @@
+﻿using Mixxfit.Admin.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mixxfit.Admin.Features.Auth
+{
+    public class AuthResponse
+    {
+        public string AccessToken { get; init; } = null!;
+        public UserDetailsDto User { get; init; } = null!;
+    }
+}
